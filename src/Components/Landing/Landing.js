8b1783159f0aeb6ad5846174a298603ddb35c89f
JJosan <nosajn123@gmail.com>
2023-01-31 19:35:42 -0800

    created buttons to test endpoints

---
+++ src/Components/Landing/Landing.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import './Landing.css';
+
+function signin() {
+  const requestOptions = {
+    credentials: 'include',
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({email: 'nosajn123@gmail.com', password: '123'})
+  }
+
+  fetch(`http://localhost:3001/login/signin`, requestOptions)
+  .then(res => res.json())
+  .then(data => console.log(data))
+}
+
+function checkAuth() {
+  fetch(`http://localhost:3001/api/users/self`)
+  .then(res => res.json())
+  .then(data => console.log(data))
+}
+
+function Landing() {
+  return (
+    <div>
+      <h1>Landing</h1>
+      <button onClick={signin}>signin</button>
+      <button onClick={checkAuth}>check if authenticated</button>
+    </div>
+  )
+}
+
+export default Landing
